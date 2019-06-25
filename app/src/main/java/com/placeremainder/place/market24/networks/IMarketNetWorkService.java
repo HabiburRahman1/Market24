142b6849e243b58bd28364f826d22105aa639fae
Md Habibur Rahman <habib.usa2014@gmail.com>
2019-06-25 15:14:19 +0600

    Login and Registration fragment and Transparentactivity created

---
+++ app/src/main/java/com/placeremainder/place/market24/networks/IMarketNetWorkService.java
@@ -0,0 +1,8 @@
+package com.placeremainder.place.market24.networks;
+
+/**
+ * Created by Habib on 25/6/19.
+ */
+
+public interface IMarketNetWorkService {
+}
---
+++ app/src/main/java/com/placeremainder/place/market24/networks/Interator.java
@@ -0,0 +1,32 @@
+package com.placeremainder.place.market24.networks;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Habib on 25/6/19.
+ */
+
+public class Interator {
+    public static final String URL      = "http://192.168.15.106";
+    public static Retrofit RETROFIT     = null;
+    public static Retrofit getClient(){
+        if(RETROFIT==null){
+            OkHttpClient client = new OkHttpClient();
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
+
+            RETROFIT = new Retrofit.Builder()
+                    .baseUrl(URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+        return RETROFIT;
+    }
+}
